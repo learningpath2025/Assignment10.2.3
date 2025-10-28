6e4ca0adc0836e6b0ca756db22db953b49a3892e
learningpath2025 <learningpath2025@outlook.com>
2025-10-28 16:00:34 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment10._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>{ "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            Console.WriteLine("The cities are :");
+            list.ForEach(city => Console.Write($"{city}, "));
+            Console.WriteLine();
+
+            var result = from city in list
+                         where city.StartsWith('A') && city.EndsWith('M')
+                         select city;
+
+            Console.WriteLine($"\nThe cities that start with 'A' and end with 'M' are :");
+            result.ToList().ForEach(city => Console.Write($"{city} "));
+            Console.ReadLine();
+        }
+    }
+}
